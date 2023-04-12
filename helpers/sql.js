0ca149cb67fb0c603fc3320fc26f3f2d29134c43
Violet Kester <kestervj89@gmail.com>
2023-04-12 14:10:15 -0400

    initial upload

---
+++ helpers/sql.js
@@ -0,0 +1,25 @@
+const { BadRequestError } = require("../expressError");
+
+// TODO: THIS NEEDS SOME GREAT DOCUMENTATION.
+/** sqlForPartialUpdate(dataToUpdate, jsToSql)
+ *
+ * - accepts object of data to be updated,
+ * -
+ */
+
+function sqlForPartialUpdate(dataToUpdate, jsToSql) {
+  const keys = Object.keys(dataToUpdate);
+  if (keys.length === 0) throw new BadRequestError("No data");
+
+  // {firstName: 'Aliya', age: 32} => ['"first_name"=$1', '"age"=$2']
+  const cols = keys.map((colName, idx) =>
+      `"${jsToSql[colName] || colName}"=$${idx + 1}`,
+  );
+
+  return {
+    setCols: cols.join(", "),
+    values: Object.values(dataToUpdate),
+  };
+}
+
+module.exports = { sqlForPartialUpdate };
