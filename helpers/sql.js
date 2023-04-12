75a5f59f5c1917a6021fbe168c981c5295c20e18
Kevin Nguyen <kayvinhnguyen@gmail.com>
2023-04-12 13:44:58 -0700

    Finished testing sql helper function

--- helpers/sql.js
+++ helpers/sql.js
@@ -3,11 +3,12 @@ const { BadRequestError } = require("../expressError");
 // TODO: THIS NEEDS SOME GREAT DOCUMENTATION.
 /** sqlForPartialUpdate(dataToUpdate, jsToSql)
  *
- * - accepts object of data to be updated,
- * -
+ * - First arg: accepts object of JSON data to be updated
+ * - Second arg: accepts JS object to be converted to SQL
  */
 
 function sqlForPartialUpdate(dataToUpdate, jsToSql) {
+  // keys = [firstName, age]
   const keys = Object.keys(dataToUpdate);
   if (keys.length === 0) throw new BadRequestError("No data");
 
---
+++ helpers/sql.test.js
@@ -0,0 +1,39 @@
+const { sqlForPartialUpdate } = require("./sql");
+const { BadRequestError } = require("../expressError");
+
+describe("sqlForPartialUpdate", function () {
+    test("Return correct data for setCols and values", function () {
+        const dataToUpdate = { firstName: 'Aliya', lastName: 'last'};
+        const jsToSql = {firstName: 'first_name', lastName: 'last_name'};
+
+        const result = sqlForPartialUpdate(dataToUpdate, jsToSql);
+        expect(result).toEqual({
+            setCols: '"first_name"=$1, "last_name"=$2',
+            values: ['Aliya', 'last']
+        });
+    });
+
+    test("Return an object with setCols and values as properties", function () {
+        const dataToUpdate = { firstName: 'Aliya', lastName: 'last'};
+        const jsToSql = {firstName: 'first_name', lastName: 'last_name'};
+
+        const result = sqlForPartialUpdate(dataToUpdate, jsToSql);
+        expect(result).toHaveProperty('setCols');
+        expect(result).toHaveProperty('values');
+    });
+
+    test("Should throw BadRequestError if dataToUpdate is empty", function () {
+        const dataToUpdate = {};
+        const jsToSql = {firstName: 'first_name', lastName: 'last_name'};
+
+        expect(() => sqlForPartialUpdate(dataToUpdate, jsToSql)).toThrow(BadRequestError);
+    });
+    
+    test("Should return camelCase properties if jsToSql is empty", function () {
+        const dataToUpdate = {firstName: 'Aliya', lastName: 'last'};
+        const jsToSql = {};
+
+        const result = sqlForPartialUpdate(dataToUpdate, jsToSql);
+        expect(result.setCols).toEqual('"firstName"=$1, "lastName"=$2');
+    });
+});
